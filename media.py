670a66402cad81f419eb3d3fdb7b89bd18402093
Marcellus Pelcher <marcell14@yahoo.com>
2015-08-04 23:01:26 -0700

    Add comments to media.py and fix pep8 findings

--- media.py
+++ media.py
@@ -1,11 +1,16 @@
 import webbrowser
 
-class Movie(object):
-  def __init__(self, title, storyline, poster_image_url, trailer_url):
-    self.title = title
-    self.storyline = storyline
-    self.poster_image_url = poster_image_url
-    self.trailer_youtube_url = trailer_url
 
-  def show_trailer(self):
-    webbrowser.open(self.trailer_url)
+class Movie(object):
+    """Data structure to hold movie information."""
+    def __init__(self, title, storyline, poster_image_url, trailer_url):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_url
+
+    def show_trailer(self):
+        """Opens a browser and goes to the trailer url that was passed
+        into the constructor.
+        """
+        webbrowser.open(self.trailer_url)
